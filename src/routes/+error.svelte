e7d04b358569d83756537b9ee986ed344b479129
bswck <bartosz@slawecki.dev>
2025-11-13 09:55:09 +0100

    Add 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { page } from '$app/state';
+
+  import Icon from '@iconify/svelte';
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    setTimeout(() => {
+      window.location.href = '/';
+    }, 5000);
+  });
+</script>
+
+<a href="/" class="justify-self-center prose prose-md m-auto text-center">
+  <h1>{page.status}</h1>
+  <p>We're going back... <Icon class="inline-block" icon="hugeicons:arrow-turn-backward" /></p>
+</a>
