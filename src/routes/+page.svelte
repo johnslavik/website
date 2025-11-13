153b053add40c0b7fcd864de58fa58c1a63a9405
bswck <bartosz@slawecki.dev>
2025-11-13 08:23:19 +0100

    Ok... I think that's it

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,18 +10,20 @@
 	<link rel="preload" as="image" href={Hero.img.src} imagesrcset={preloadSrcSet} />
 </svelte:head>
 
-<div class="relative flex min-h-60 flex-row not-sm:flex-col sm:h-screen">
+<!-- You can tell this took me a lot of time to get right ;) -->
+
+<div class="relative grid w-screen h-screen min-h-60 sm:grid-cols-[auto_1fr] not-sm:grid-rows-[auto_1fr]">
 	<About />
-	<div class="relative flex w-full justify-center overflow-clip">
+	<div class="relative flex w-full justify-center sm:justify-end overflow-hidden min-w-82">
 		<article
 			id="socials"
-			class="absolute left-8 prose prose-sm py-8 not-sm:top-0 not-sm:rounded-b-[30%] not-sm:p-4 not-sm:mix-blend-difference not-sm:prose-invert sm:fixed sm:bottom-0 sm:rounded-t-[30%]"
+			class="absolute left-8 prose prose-sm py-8 not-sm:top-0 not-sm:rounded-b-[30%] not-sm:mix-blend-difference not-sm:prose-invert sm:fixed sm:bottom-0 sm:rounded-t-[30%]"
 		>
 			<Socials />
 		</article>
 		<enhanced:img
 			id="hero"
-			class="size-full w-screen object-cover transition-all duration-300"
+			class="h-[75vh] object-cover transition-all duration-300 sm:size-full"
 			src={Hero}
 			alt=""
 		/>
@@ -32,17 +34,9 @@
 	@reference 'tailwindcss';
 	@plugin '@tailwindcss/typography';
 
-	@media (max-height: 32rem) and (min-width: 40rem) {
+	@media (max-height: 30rem) and (min-width: 40rem) {
 		#socials {
 			@apply absolute top-0 mix-blend-difference prose-invert;
 		}
-	}
-
-	@media not (width >= 40rem) {
-		#hero {
-			height: 50vh;
-			height: 50dvh;
-		}
-
 	}
 </style>
